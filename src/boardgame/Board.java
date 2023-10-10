e015e8ffb5d2fab4320e51b4201ea61d42c57618
Carlos Cardoso <cvcardos5@gmail.com>
2023-10-10 08:48:11 -0300

    Adicionado King e Rook

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,6 +38,7 @@ public class Board {
     public void placePiece(Piece piece, Position position) {
         if (piece(position) == null) {
             pieces[position.getRow()][position.getColumn()] = piece;
+            piece.position = position;
         }
     }
 
